7d574751057900b614a0b45a6d75253fe7abeec1
Yunus <yunus@initialcode.com.tr>
2023-07-20 16:34:10 +0300

    User defaults helper was added

--- NetmeraFintech/UI/Onboarding/OnboardingVC.swift
+++ NetmeraFintech/UI/Onboarding/OnboardingVC.swift
@@ -25,6 +25,10 @@ class OnboardingVC: UIViewController {
         signInButton.addTarget(self, action: #selector(signInButtonClicked), for: .touchUpInside)
         getStartedButton.addTarget(self, action: #selector(getStartedButtonClicked), for: .touchUpInside)
 
+        if let email = UserDefaultsHelper.email() {
+            emailTextField.text = email
+        }
+
         Netmera.setEnabledPopupPresentation(true)
         Netmera.setEnabledInAppMessagePresentation(true)
     }
@@ -39,6 +43,7 @@ class OnboardingVC: UIViewController {
         if let email = emailTextField.text, !email.isEmpty {
             viewModel.updateUserAndSendSignInEvent(email: email)
             message = "The user update is completed and the sign-In event was sent."
+            UserDefaultsHelper.email(value: email)
         }
         showDialog(message: message)
     }
