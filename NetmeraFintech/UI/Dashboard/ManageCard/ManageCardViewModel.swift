5ce697f420b7387981dbf4aa5816ad65b12454f9
Yunus <yunus@initialcode.com.tr>
2023-08-01 14:33:20 +0300

    created enum manage options

--- NetmeraFintech/UI/Dashboard/ManageCard/ManageCardViewModel.swift
+++ NetmeraFintech/UI/Dashboard/ManageCard/ManageCardViewModel.swift
@@ -6,12 +6,40 @@ import Foundation
 class ManageCardViewModel {
     private var card: Card?
 
+    enum ManageOptions {
+        case FreezeCard
+        case ForgotPin
+        case Settings
+        case Support
+
+        var index: Int {
+            switch self {
+            case .FreezeCard: return 0
+            case .ForgotPin: return 1
+            case .Settings: return 2
+            case .Support: return 3
+            }
+        }
+    }
+
     let options = [
-        ManageOption(index: 0, title: "Freeze card", description: "Lost or stolen? Freeze it here", color: Color.pink),
-        ManageOption(index: 1, title: "Forgot your PIN?", description: "Lost or stolen? Freeze it here", color: Color.purple),
-        ManageOption(index: 2, title: "Settings", description: "Terminate and more", color: Color.blue),
-        ManageOption(index: 3, title: "Support", description: "Contact us about your card", color: Color.green),
-    ]
+        ManageOption(index: ManageOptions.FreezeCard.index,
+                     title: "Freeze card",
+                     description: "Lost or stolen? Freeze it here",
+                     color: Color.pink),
+        ManageOption(index: ManageOptions.ForgotPin.index,
+                     title: "Forgot your PIN?",
+                     description: "Lost or stolen? Freeze it here",
+                     color: Color.purple),
+        ManageOption(index: ManageOptions.Settings.index,
+                     title: "Settings",
+                     description: "Terminate and more",
+                     color: Color.blue),
+        ManageOption(index: ManageOptions.Support.index,
+                     title: "Support",
+                     description: "Contact us about your card",
+                     color: Color.green),
+    ].sorted(by: { $0.index < $1.index })
 
     func setCard(card: Card) {
         self.card = card
@@ -19,10 +47,10 @@ class ManageCardViewModel {
 
     func onOptionClick(index: Int) {
         switch index {
-        case 0: sendFreezeCardEvent()
-        case 1: sendForgotPinEvent()
-        case 2: sendCardSettingsEvent()
-        case 3: sendSupportEvent()
+        case ManageOptions.FreezeCard.index: sendFreezeCardEvent()
+        case ManageOptions.ForgotPin.index: sendForgotPinEvent()
+        case ManageOptions.Settings.index: sendCardSettingsEvent()
+        case ManageOptions.Support.index: sendSupportEvent()
         default: break
         }
     }
